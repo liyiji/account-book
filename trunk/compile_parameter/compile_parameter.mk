469a1203e20608014e18e8cd2e1e94ce7d2cfe4f
liyijipersonal <liyijipersonal@cb5c7fab-9a5d-ead0-01e1-64e3a19da7e9>
2012-02-03 08:59:13 +0000

    编译参数独立出来

---
+++ trunk/compile_parameter/compile_parameter.mk
@@ -0,0 +1,15 @@
+
+QMAKE_CC = gcc
+QMAKE_CXX = g++
+QMAKE_LINK = g++
+
+DESTDIR = ../../../bin_accountBook
+
+OBJECTS_DIR = .obj
+MOC_DIR     = .moc
+RCC_DIR     = .rcc
+UI_DIR      = .uidir
+
+CONFIG  +=  warn_on
+
+#DEFINES += DEBUG_VERSION
